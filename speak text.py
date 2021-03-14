8078845825bf9f68d624da97f7744ca001484b3d
aniket941 <78209357+aniket941@users.noreply.github.com>
2021-03-14 11:16:59 +0530

    Update speak text.py

--- speak text.py
+++ speak text.py
@@ -7,7 +7,8 @@ engine = pyttsx3.init()
 
 # .say() function is used to speak the text you have written 
 # inside the function
-engine.say("Anything you write here will be spoken by your computer during the running of the program")
+#whatever we write here computer will speak
+engine.say("hello Aniket Singh")
 
 # this is used to process and run the program commands
 engine.runAndWait()
